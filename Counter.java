10d65cc28073f480d4df2cd46412d40451994bd3
MansillSmith <mansill.smith@xtra.co.nz>
2020-05-30 17:19:32 +1200

    Counter: creates a form with the input image

--- Counter.java
+++ Counter.java
@@ -21,12 +21,13 @@ public class Counter{
     static int numInputs = 1;
 
     public static void main(String[] args){
-        if(args.length != numInputs){
+        if(args.length == numInputs){
             Counter counter = new Counter(args[0]);
         }
     }
 
     public Counter(String filename){
+        //Read in the file
         BufferedImage img = null;
         try{
             img=ImageIO.read(new File(filename));
@@ -34,12 +35,20 @@ public class Counter{
         catch (IOException e){
             img = null;
         }
-        
+
         if(img == null){
             System.err.println("Invalid file");
         }
         else{
+            //Creates a form with the image
+            JFrame frame = new JFrame();
+            frame.setLayout(new FlowLayout());
+            frame.getContentPane().setLayout(new FlowLayout());
+            frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+            frame.setSize(img.getWidth(),img.getHeight());
 
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         }
     }
 }

d0b38b135fde3bef49661e483f0ce2553ab5aef4
MansillSmith <mansill.smith@xtra.co.nz>
2020-05-30 16:05:39 +1200

    Counter: Reads a file into a buffered image

--- Counter.java
+++ Counter.java
@@ -1,9 +1,32 @@
+/*
+Mansill Smith
+ID: 1341291
+
+Alex Grant
+ID: 1350168
+*/
+
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Counter{
+
+    //The only input is the image
+    static int numInputs = 1;
+
     public static void main(String[] args){
-        Counter counter = new Counter();
+        if(arg.length != numInputs){
+            Counter counter = new Counter(args[0]);
+        }
     }
 
-    public Counter(){
-
+    public Counter(String filename){
+        BufferedImage img=ImageIO.read(new File(filename));
     }
 }
